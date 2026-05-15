64bfe357c313ce7a47df3e57bdb7026f4f5c32e4
Jenny Teresa Pescador Bañol <jenny.pescador@ad.gruporaven.com.co>
2026-05-15 15:12:17 -0500

    first commit

--- script.js
+++ script.js
@@ -1,66 +1,53 @@
-const paginas = document.querySelectorAll(".pagina");
-
+const botonIniciar = document.getElementById("iniciarHistoria");
 const siguiente = document.getElementById("siguiente");
-
 const anterior = document.getElementById("anterior");
+const volverInicio = document.getElementById("volverInicio");
 
-let paginaActual = 1;
+const paginas = document.querySelectorAll(".pagina");
+const audio = document.getElementById("audioCampo");
 
-/* ========================= */
-/* BOTON INICIAR */
-/* ========================= */
+let paginaActual = 0;
 
-const iniciarHistoria = document.getElementById("iniciarHistoria");
-
-iniciarHistoria.addEventListener("click", ()=>{
-
-    paginas[0].classList.remove("active");
-
-    paginaActual = 1;
-
-    paginas[1].classList.add("active");
-
-});
-
-/* MOSTRAR PAGINA */
-
-function mostrarPagina(indice){
-
-    paginas.forEach((pagina)=>{
-
-        pagina.classList.remove("active");
-
-    });
-
-    paginas[indice].classList.add("active");
+function mostrarPagina(index) {
+    paginas.forEach(p => p.classList.remove("active"));
+    paginas[index].classList.add("active");
 }
 
+/* INICIAR */
+botonIniciar.addEventListener("click", () => {
+
+    document.body.classList.remove("portada-activa");
+    document.body.classList.add("historia-activa");
+
+    paginaActual = 0;
+    mostrarPagina(paginaActual);
+
+    audio.volume = 0.4;
+    audio.play();
+});
+
 /* SIGUIENTE */
-
-siguiente.addEventListener("click", ()=>{
-
-    paginaActual++;
-
-    if(paginaActual >= paginas.length){
-
-        paginaActual = paginas.length -1;
-    }
-
+siguiente.addEventListener("click", () => {
+    if (paginaActual < paginas.length - 1) paginaActual++;
     mostrarPagina(paginaActual);
 });
 
 /* ANTERIOR */
-
-anterior.addEventListener("click", ()=>{
-
-    paginaActual--;
-
-    if(paginaActual < 0){
-
-        paginaActual = 0;
-    }
-
+anterior.addEventListener("click", () => {
+    if (paginaActual > 0) paginaActual--;
     mostrarPagina(paginaActual);
 });
 
+/* VOLVER INICIO */
+volverInicio.addEventListener("click", () => {
 
+    audio.pause();
+    audio.currentTime = 0;
+
+    document.body.classList.add("portada-activa");
+    document.body.classList.remove("historia-activa");
+
+    paginas.forEach(p => p.classList.remove("active"));
+
+    paginaActual = 0;
+});
